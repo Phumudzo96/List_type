9bf8b5a5ffc4b3627db9d6791313d49ba885aa79
Phumudzo <103152398+Phumudzo96@users.noreply.github.com>
2023-02-15 00:25:20 +0200

    Add files via upload

---
+++ list_types.py
@@ -0,0 +1,13 @@
+friends_list = ["Ferd", "Harry", "Larry"]  # Create a variable with a list of sting names
+print(friends_list)
+
+print(friends_list[0])    # Print out the name at index 0
+print(friends_list[2])    # Print out the name at index 2
+print(len(friends_list))   # Print out the number of names in the list
+
+friends_age = [23, 26, 26]                # create a variable  with a list of numbers
+print(friends_age)
+
+print(f"{friends_age[0]}  {friends_list[0]}, {friends_age[1]}  {friends_list[1]}, {friends_age[2]}  {friends_list[2]}")
+
+# Print out the names and numbers from first to last as in their variables
